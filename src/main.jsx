788b51faec059ac4f72e9c66120afe108dc331b2
DevDesignJustin <devdesignjustin@gmail.com>
2026-04-12 16:15:37 -0400

    analitics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { Analytics } from "@vercel/analytics/react";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
-)
+);
